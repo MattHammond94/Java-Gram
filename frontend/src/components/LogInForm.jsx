2335ad1b43b82475af14ebd1f606d8c139765dd0
Matt Hammond <mjhammond94@gmail.com>
2023-10-30 12:45:44 +0000

    Add SignUpForm - amend styling

--- frontend/src/components/LogInForm.jsx
+++ frontend/src/components/LogInForm.jsx
@@ -7,17 +7,20 @@ const LogInForm = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     console.log('submit');
+    console.log(username);
+    console.log(password);
   }
 
   return (
-    <div className="contact-form">
+    <div className="login-form">
       <form onSubmit={ submitHandler }>
+        <h1>Log In</h1>
         <label>Username:</label>
         <input type="text" name="username" value={ username } onChange={ (e) => setUsername(e.target.value) }/>
         <label>Password:</label>
-        <input type="text" name="password" value={ password } onChange={ (e) => setPassword(e.target.value) }/>
+        <input type="password" name="password" value={ password } onChange={ (e) => setPassword(e.target.value) }/>
         <button>Sign In</button>
-        <p>New to Java-Gram? <a href='/'>register here</a></p>
+        <p>New to Java-Gram? <a href='/'>Register here</a></p>
       </form>
     </div>
   )
--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react';
 
 const SignUpForm = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log('submit');
+    console.log(username);
+    console.log(password);
+  }
+
   return (
-    <div>SignUpForm</div>
+    <div className="signup-form">
+      <form onSubmit={ submitHandler }>
+        <h1>Sign Up</h1>
+        <label>E-mail Address:</label>
+        <input type="text" name="email" value={ email } onChange={ (e) => setEmail(e.target.value) }/>
+        <label>Username:</label>
+        <input type="text" name="username" value={ username } onChange={ (e) => setUsername(e.target.value) }/>
+        <label>Password:</label>
+        <input type="password" name="password" value={ password } onChange={ (e) => setPassword(e.target.value) }/>
+        <label>Confirm Password:</label>
+        <input type="password" name="confirmPassword" value={ confirmPassword } onChange={ (e) => setConfirmPassword(e.target.value) }/>
+        <button>Sign In</button>
+        <p>Already registered? <a href='/'>Log in here</a></p>
+      </form>
+    </div>
   )
 }
 
