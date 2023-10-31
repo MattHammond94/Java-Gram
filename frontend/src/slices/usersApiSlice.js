e3e76cd70f00a0f541bcd59c46afe7c842517b05
Matt Hammond <mjhammond94@gmail.com>
2023-10-31 12:23:12 +0000

    Add all endpoints to usersApiSlice

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -17,10 +17,37 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         method: 'POST'
       }),
     }),
+    createUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/new`,
+        method: 'POST',
+        body: data
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/user`,
+        method: 'PUT',
+        body: data
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/user`,
+        method: 'DELETE',
+      }),
+    }),
+    getUserInfo: builder.query({
+      url: `${USERS_URL}/user`
+    }),
   }),
 });
 
 export const {
   useLoginMutation,
-  useLogoutMutation
+  useLogoutMutation,
+  useCreateUserMutation,
+  useUpdateUserMutation,
+  useDeleteUserMutation,
+  useGetUserInfoQuery
 } = usersApiSlice;
