bfb41df21791aae0ecbfb276ce02027feef7969e
Matt Hammond <mjhammond94@gmail.com>
2023-11-01 15:50:26 +0000

    Resolve issues with running tests

---
+++ backend/__tests__/models/comment.spec.js
@@ -0,0 +1,17 @@
+// import Comment from '../../models/commentModel.js';
+// import User from '../../models/userModel.js';
+
+// describe('Comment Model', () => {
+//   const user = new User({
+    
+//   })
+
+//   const comment = new Comment({
+//     caption: 'This is a comment',
+//     user: ''
+//   })
+
+//   it('Should have a caption attribute', () => {
+
+//   })
+// })
