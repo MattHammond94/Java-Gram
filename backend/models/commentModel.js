e5318fefa9b38625eaf2259c33b52d3c24dbf4f3
Matt Hammond <mjhammond94@gmail.com>
2023-11-01 15:08:25 +0000

    Add comment and post models to backend

---
+++ backend/models/commentModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const commentSchema = new Schema({
+  caption: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId, 
+    ref: "User",
+    required: true
+  },
+},
+{
+  timestamps: true
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+export default Comment;
---
+++ backend/models/postModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  image: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  caption: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId, 
+    ref: "User",
+    required: true
+  },
+  comments: [{
+    type: Schema.Types.ObjectId, 
+    ref: "Comment",
+    default: []
+  }],
+  likedBy: {
+    type: Array,
+    default: []
+  }
+},
+{
+  timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
