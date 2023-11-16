39ca897bc23c472b4bd73734d0388251d9610437
Matt Hammond <mjhammond94@gmail.com>
2023-11-16 22:51:05 +0000

    Refactor code into UserPageHeader component

--- frontend/src/components/UserPageHeader.jsx
+++ frontend/src/components/UserPageHeader.jsx
@@ -1,7 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import { useGetSelectedUserInfoQuery } from "../slices/usersApiSlice";
+import Loader from "./Loader";
 
 const UserPageHeader = ({ username }) => {
+  const navigate = useNavigate();
+  const { data: userInfo, error: userError, isLoading: userInfoLoading } = useGetSelectedUserInfoQuery(`${username}`);
+
+  const handleNavigate = () => {
+    navigate('/feed')
+  }
+
+  if (userInfoLoading) {
+    return <Loader />
+  }
+
+  if (userError) {
+    return <div>Error: {userError.message}</div>;
+  }
+
+  const total = 0
+
   return (
-    
+    <div className='userHeader'>
+      <div className='leftSide'>
+        <button>Update</button>
+        <img src={`${userInfo.profilePicture}`} alt='Users personal profile picture' />
+      </div>
+      <div className='middle'>
+        <div className='middleTop'>
+          <h1>{ username }</h1>
+        </div>
+        <div className='middleMiddle'>
+          <div className='posts'>
+            <p>{`${total}`}</p>
+            <p>Posts</p>
+          </div>
+          <div className='followers'>
+            <p>{`${userInfo.followers.length}`}</p>
+            <p>Followers</p>
+          </div>
+          <div className='Following'>
+            <p>{`${userInfo.following.length}`}</p>
+            <p>Following</p>
+          </div>
+        </div>
+      </div>
+      <div className='rightSide'>
+        <button onClick={ handleNavigate }>Home</button>
+        <button>Settings</button>
+        <button>Create Post</button>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -1,17 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useGetSelectedUserInfoQuery, useCheckUsernameQuery } from "../slices/usersApiSlice";
+import { useCheckUsernameQuery } from "../slices/usersApiSlice";
 import Loader from "../components/Loader";
-import { useNavigate } from "react-router-dom";
 import NotFoundPage from "../pages/NotFoundPage";
+import UserPageHeader from "../components/UserPageHeader";
 
 // Will consist of two components - A header and a gallery
-
-// Header will contain:
-// profile image (a picture that when selected)
-// List of followers
-// List of following
-// Button to follow/unfollow
-// A home button that redirects to the feed
 
 // Gallery will contain: 
 // a gallery of thumbnails from all that specific users Posts 
@@ -19,18 +12,15 @@ import NotFoundPage from "../pages/NotFoundPage";
 // IF user is logged in they can delete their image here
 // On delete image will need to also be deleted from the cloud library so will need to implement backend for this.
 
-
 // IF the user selected(":id" in req params/link) === current users ID then extra elements will be rendered to this page 
 // Extra elements will be a drop down menu that includes updating info.
 
 
 const UserPage = () => {
-  const navigate = useNavigate();
   const { username } = useParams();
   const { data: userExists, error: userExistsError, isLoading: userExistsLoading } = useCheckUsernameQuery(`${username}`);
-  const { data: userInfo, error: userError, isLoading: userInfoLoading } = useGetSelectedUserInfoQuery(`${username}`);
 
-  if (userExistsLoading || userInfoLoading) {
+  if (userExistsLoading) {
     return <Loader />
   }
 
@@ -38,47 +28,12 @@ const UserPage = () => {
     return <NotFoundPage />
   }
 
-  if (userError || userExistsError) {
-    return <div>Error: {userError.message || userExists.error}</div>;
+  if (userExistsError) {
+    return <div>Error: {userExistsError.message}</div>;
   }
-
-  const handleNavigate = () => {
-    navigate('/feed')
-  }
-  
-  const total = 0
 
   return (
-    <div className='userHeader'>
-      <div className='leftSide'>
-        <button>Update</button>
-        <img src={`${userInfo.profilePicture}`} alt='Users personal profile picture' />
-      </div>
-      <div className='middle'>
-        <div className='middleTop'>
-          <h1>{ username }</h1>
-        </div>
-        <div className='middleMiddle'>
-          <div className='posts'>
-            <p>{`${total}`}</p>
-            <p>Posts</p>
-          </div>
-          <div className='followers'>
-            <p>{`${userInfo.followers.length}`}</p>
-            <p>Followers</p>
-          </div>
-          <div className='Following'>
-            <p>{`${userInfo.following.length}`}</p>
-            <p>Following</p>
-          </div>
-        </div>
-      </div>
-      <div className='rightSide'>
-        <button onClick={ handleNavigate }>Home</button>
-        <button>Settings</button>
-        <button>Create Post</button>
-      </div>
-    </div>
+    <UserPageHeader username={ username }/>
   )
 }
 
