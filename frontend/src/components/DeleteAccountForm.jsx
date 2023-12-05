d1fabf24a63880c3623c91ecb2967a1b0da6defb
Matt Hammond <mjhammond94@gmail.com>
2023-12-05 21:15:17 +0000

    Update DeleteAccountForm component

--- frontend/src/components/DeleteAccountForm.jsx
+++ frontend/src/components/DeleteAccountForm.jsx
@@ -1,9 +1,35 @@
+import { useDeleteUserMutation, useLogoutMutation } from "../slices/usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { logout } from "../slices/authSlice";
+import { useDispatch } from "react-redux";
+import Loader from "./Loader";
 
 const DeleteAccountForm = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [removeCookie, { isLoading: cookieRemoverLoading }] = useLogoutMutation();
+  const [deleteUser, { isLoading: deleteUserLoading }] = useDeleteUserMutation();
+
+  const handleAccountDeletion = async () => {
+    const response = await deleteUser();
+
+    if (response) {
+      try {
+        await removeCookie();
+        dispatch(logout());
+        navigate('/')
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  }
+
   return (
-    <div className="formTemplate">
-      <h1>DeleteAccountForm</h1>
-      {/* Warning message: Are you sure you   -  DeleteAccountButton*/}
+    <div className="formTemplate deleteAccountForm">
+      <h1>Remove Account</h1>
+      <p><span>WARNING:</span> Deleting your account will irreversibly remove all your current posts and data.</p>
+      <p>Are you sure you want to delete your account?</p>
+      { cookieRemoverLoading || deleteUserLoading ? <button><Loader /></button> : <button onClick={ handleAccountDeletion }>Remove Account</button> }
     </div>
   )
 }
