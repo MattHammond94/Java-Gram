c14478b06ce839b9d8dd5a1f1aa42fa07f23ecc0
Matt Hammond <mjhammond94@gmail.com>
2023-12-14 15:43:20 +0000

    Update Loader component

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,21 @@
-const Loader = () => {
+const Loader = ({ variant }) => {
+  let className;
+
+  if (variant === 'large') {
+    className = "largeLoader";
+  } else {
+    className = "loader";
+  }
+
   return (
-    <div className="loader">
-      <div></div>
-      <div></div>
-    </div>
+    <>
+      <div className={className}>
+        <div></div>
+        <div></div>
+      </div>
+      { variant === 'large' ? <p>Loading...</p> : null }
+    </>
+
   )
 }
 
