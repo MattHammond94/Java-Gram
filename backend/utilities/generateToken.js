8eb50c25dd65cf63ef42a66d7c6bfb37b5caf5d3
Matt Hammond <mjhammond94@gmail.com>
2024-01-22 22:40:15 +0000

    Change sameSite config in generateToken function

--- backend/utilities/generateToken.js
+++ backend/utilities/generateToken.js
@@ -11,9 +11,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     // secure: process.env.NODE_ENV !== 'development',
-    secure: false,
-    sameSite: 'strict',
-    domain: 'java-gram-backend.onrender.com'
+    secure: true,
+    sameSite: 'None',
     // maxAge: 1 * 60 * 1000
   });
 }
