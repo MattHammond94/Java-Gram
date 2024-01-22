0c5b9bafb2ccde4249558c352bbdaa9f384c84c3
Matt Hammond <mjhammond94@gmail.com>
2024-01-22 22:24:49 +0000

    Add headers for baseQuery in apiSlice

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://java-gram-backend.onrender.com/api' });
+const baseQuery = fetchBaseQuery({ 
+  baseUrl: 'https://java-gram-backend.onrender.com/api',
+  prepareHeaders: (headers) => {
+    headers.set('credentials', 'include');
+    return headers;
+  },
+});
 
 export const apiSlice = createApi({
   baseQuery,
