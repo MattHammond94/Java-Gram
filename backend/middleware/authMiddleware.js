0547902a7ecaf796f9ae757fd90d7793e4bc50ed
Matt Hammond <mjhammond94@gmail.com>
2024-01-22 16:54:11 +0000

    Add further logs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,9 +6,12 @@ const protect = asyncHandler(async (req, res, next) => {
   let token;
   token = req.cookies.jwt;
 
+  console.log(token);
+
   if (token) {
     try {
       console.log(JWT_SECRET)
+      console.log(process.env.JWT_SECRET)
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       console.log(decoded.userId)
       req.user = await User.findById(decoded.userId).select('-password');
