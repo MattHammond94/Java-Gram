b6b10da7deac6de698b4d02c16e1c29a5b5ba5ad
Matt Hammond <mjhammond94@gmail.com>
2024-01-22 10:30:37 +0000

    Update config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'https://java-gram-backend.onrender.com',
         changeOrigin: true
       },
     },
