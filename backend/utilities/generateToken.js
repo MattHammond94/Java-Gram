46eb7e388d5f41e3616c67b557c7601d5a28c38b
Matt Hammond <mjhammond94@gmail.com>
2024-01-22 17:57:39 +0000

    Remove secure and strict values from cookie generator

--- backend/utilities/generateToken.js
+++ backend/utilities/generateToken.js
@@ -10,8 +10,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
+    // secure: process.env.NODE_ENV !== 'development',
+    // sameSite: 'strict',
     // maxAge: 1 * 60 * 1000
   });
 }
